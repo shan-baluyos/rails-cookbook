5cbcb663f9736a9645a6f4426903f7512307b1cb
Shan Baluyos <shan.baluyos21@gmail.com>
2024-09-30 10:22:50 +0100

    added bookmark model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_30_090824) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_30_092128) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "recipe_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_bookmarks_on_category_id"
+    t.index ["recipe_id"], name: "index_bookmarks_on_recipe_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -28,4 +38,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_30_090824) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "bookmarks", "categories"
+  add_foreign_key "bookmarks", "recipes"
 end
