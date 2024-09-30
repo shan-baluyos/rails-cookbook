2b877d523061e2a2a941c886d035282dc9cbdf69
Shan Baluyos <shan.baluyos21@gmail.com>
2024-09-30 11:05:01 +0100

    added validations and associations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :recipe
   belongs_to :category
+
+  validates :recipe_id, uniqueness: { scope: :category_id }
+  validates :comment, lenght: { minimum: 6, too_short: "must have at least 6 characters" }
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,9 @@
 class Recipe < ApplicationRecord
+  has_many :bookmarks
+
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :rating, inclusion: { in: (0..10) }
+  # another way for the rating:
+  # validates :rating, numercality { greater than... check notes}
 end
