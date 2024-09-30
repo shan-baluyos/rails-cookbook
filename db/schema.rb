a9d25e1d3df3f14d2bb9df6274a13303d35092e5
Shan Baluyos <shan.baluyos21@gmail.com>
2024-09-30 10:13:39 +0100

    added category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_30_085839) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_30_090824) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
